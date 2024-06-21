2548b5e99617e1d2d257fc052b288564ee40eef7
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-06-21 10:44:32 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing';
+import Hero from './Hero';
+import Business from './Business';
+import Button from './Button';
+import CTA from './CTA';
+import Clients from './Clients';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import NavBar from './NavBar';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import CardDeal from './CardDeal';
+import GetStarted from './GetStarted';
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    NavBar,
+    Stats,
+    Testimonials,
+};
